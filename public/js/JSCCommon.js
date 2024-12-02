a31ee660197e68fbae053b0b272e029e2a369ab6
JulietBrn <shishkina.ja@gmail.com>
2024-12-02 14:24:36 +0100

    feat: add blocks

--- public/js/JSCCommon.js
+++ public/js/JSCCommon.js
@@ -195,10 +195,10 @@ class JSCCommon {
 		InputTel.forEach(element =>
 			element.setAttribute(
 				"pattern",
-				"[+][0-9]{1}[(][0-9]{3}[)][0-9]{3}-[0-9]{2}-[0-9]{2}"
+				"[+][7]{1}[(][0-9]{3}[)][0-9]{3}-[0-9]{2}-[0-9]{2}"
 			)
 		);
-		Inputmask({mask: "+9(999)999-99-99", showMaskOnHover: false}).mask(
+		Inputmask({mask: "+7(999)999-99-99", showMaskOnHover: false}).mask(
 			InputTel
 		);
 	}
